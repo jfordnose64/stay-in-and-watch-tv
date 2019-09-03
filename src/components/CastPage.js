423fa8cdb195479011114d3b9af05a5666e5472e
jackson ford <jfordnose64@gmail.com>
2019-09-03 12:09:08 -0400

    added random feature

--- src/components/CastPage.js
+++ src/components/CastPage.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const CastPage = () => {
+const CastPage = props => {
   const [character, setNewCharacter] = useState([])
+
+  const [newId, setNewId] = useState(props.match.params.location.topMovies.id)
+
   const fetchCast = async () => {
     const resp = await axios.get(
-      `https://api.themoviedb.org/3/tv/${this.state.setShowId}/credits?api_key=6ace706710ef6a37e0b50b31f5814c90&language=en-US`
+      `https://api.themoviedb.org/3/tv/100/credits?api_key=6ace706710ef6a37e0b50b31f5814c90&language=en-US`
     )
     setNewCharacter(resp.data.cast)
-    console.log(character)
   }
   useEffect(() => {
     fetchCast()
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import CastPage from './CastPage'
@@ -7,6 +7,7 @@ import CastPage from './CastPage'
 const HomePage = () => {
   const [topMovies, newTopMovies] = useState([])
   const [showId, setShowId] = useState('')
+  const [randomMovie, setRandomMovie] = useState('')
 
   const fetchMovies = async () => {
     const resp = await axios.get(
@@ -14,6 +15,7 @@ const HomePage = () => {
     )
     newTopMovies(resp.data.results)
     setShowId(resp.data.results[0].id)
+    setRandomMovie(resp.data.results[Math.floor(Math.random() * 19)])
   }
 
   useEffect(() => {
@@ -22,18 +24,28 @@ const HomePage = () => {
 
   return (
     <Router>
+      <article className="randomMovie">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${randomMovie.poster_path}`}
+        />
+        <h2>{randomMovie.original_name}</h2>
+        <p>{randomMovie.overview}</p>
+      </article>
       <h3>
         {topMovies.map(movies => {
           return (
-            <li className="top-movies">
+            <li className="top-movies" key={topMovies.id}>
               <header key={movies.id}>
                 <img
                   src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
                   alt="Movie Poster"
                 />
+                <p>{movies.id}</p>
                 <h3>{movies.original_name}</h3>
                 <p className="movie-desc">{movies.overview}</p>
-                <Link to="/tv">Cast</Link>
+                <Link to={{ pathname: '/tv', props: [{ topMovies }] }}>
+                  View The Cast
+                </Link>
                 <Route exact path="/tv" component={CastPage}></Route>
               </header>
             </li>
